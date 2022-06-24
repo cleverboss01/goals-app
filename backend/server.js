ad907b2b8878a7e05eef54d2e828f52402d8a840
cleverboss01 <ndubeansdaniel97@gmail.com>
2022-06-24 15:20:02 +0100

    MERN STACK GOAL APP

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const routes = require("./routes/goalRoutes");
+const userRoutes = require("./routes/userRoutes");
+
+connectDB();
+
+//initialize app
+const app = express();
+
+//middleware to use body req
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", routes);
+app.use("/api/users", userRoutes);
+
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
